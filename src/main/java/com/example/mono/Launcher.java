31db4b9573b61aca1924d33fee2df84d51a25243
koloseki <koloseki@proton.me>
2026-04-12 20:44:23 +0200

    Initial commit

---
+++ src/main/java/com/example/mono/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.mono;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(com.example.mono.HelloApplication.class, args);
+    }
+}
